06101149fd03ddff4e1ffd219cbea47075808259
DevExpressExampleBot <fakeEmail>
2022-09-30 11:40:17 +0400

    Source auto update [skip ci]

--- VB/CustomGroupIntervals/Form1.Designer.vb
+++ VB/CustomGroupIntervals/Form1.Designer.vb
@@ -1,48 +1,50 @@
-﻿Namespace CustomGroupIntervals
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace CustomGroupIntervals
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-            Dim FieldInfo1 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo2 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo3 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo4 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo5 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo6 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo7 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo8 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo9 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim FieldInfo10 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
-            Dim ExcelWorksheetSettings1 As DevExpress.DataAccess.Excel.ExcelWorksheetSettings = New DevExpress.DataAccess.Excel.ExcelWorksheetSettings()
-            Dim ExcelSourceOptions1 As DevExpress.DataAccess.Excel.ExcelSourceOptions = New DevExpress.DataAccess.Excel.ExcelSourceOptions(ExcelWorksheetSettings1)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(CustomGroupIntervals.Form1))
+            Dim fieldInfo1 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo2 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo3 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo4 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo5 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo6 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo7 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo8 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo9 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim fieldInfo10 As DevExpress.DataAccess.Excel.FieldInfo = New DevExpress.DataAccess.Excel.FieldInfo()
+            Dim excelWorksheetSettings1 As DevExpress.DataAccess.Excel.ExcelWorksheetSettings = New DevExpress.DataAccess.Excel.ExcelWorksheetSettings()
+            Dim excelSourceOptions1 As DevExpress.DataAccess.Excel.ExcelSourceOptions = New DevExpress.DataAccess.Excel.ExcelSourceOptions(excelWorksheetSettings1)
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.excelDataSource1 = New DevExpress.DataAccess.Excel.ExcelDataSource()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
+            ' 
+            ' pivotGridControl1
+            ' 
             Me.pivotGridControl1.DataSource = Me.excelDataSource1
             Me.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill
             Me.pivotGridControl1.Location = New System.Drawing.Point(0, 0)
@@ -50,66 +52,64 @@
             Me.pivotGridControl1.OptionsData.DataProcessingEngine = DevExpress.XtraPivotGrid.PivotDataProcessingEngine.Optimized
             Me.pivotGridControl1.Size = New System.Drawing.Size(800, 450)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'excelDataSource1
-            '
-            Me.excelDataSource1.FileName = "\Data\SalesPerson2.xlsx"
+            ' 
+            ' excelDataSource1
+            ' 
+            Me.excelDataSource1.FileName = "Data\SalesPerson2.xlsx"
             Me.excelDataSource1.Name = "excelDataSource1"
             Me.excelDataSource1.ResultSchemaSerializable = resources.GetString("excelDataSource1.ResultSchemaSerializable")
-            FieldInfo1.Name = "CategoryName"
-            FieldInfo1.OriginalName = "CategoryName"
-            FieldInfo1.Type = GetType(String)
-            FieldInfo2.Name = "Country"
-            FieldInfo2.OriginalName = "Country"
-            FieldInfo2.Type = GetType(String)
-            FieldInfo3.Name = "ProductName"
-            FieldInfo3.OriginalName = "ProductName"
-            FieldInfo3.Type = GetType(String)
-            FieldInfo4.Name = "Sales Person"
-            FieldInfo4.OriginalName = "Sales Person"
-            FieldInfo4.Type = GetType(String)
-            FieldInfo5.Name = "OrderDate"
-            FieldInfo5.OriginalName = "OrderDate"
-            FieldInfo5.Type = GetType(Date)
-            FieldInfo6.Name = "OrderID"
-            FieldInfo6.OriginalName = "OrderID"
-            FieldInfo6.Type = GetType(Double)
-            FieldInfo7.Name = "Quantity"
-            FieldInfo7.OriginalName = "Quantity"
-            FieldInfo7.Type = GetType(Double)
-            FieldInfo8.Name = "Discount"
-            FieldInfo8.OriginalName = "Discount"
-            FieldInfo8.Type = GetType(Double)
-            FieldInfo9.Name = "Extended Price"
-            FieldInfo9.OriginalName = "Extended Price"
-            FieldInfo9.Type = GetType(Double)
-            FieldInfo10.Name = "UnitPrice"
-            FieldInfo10.OriginalName = "UnitPrice"
-            FieldInfo10.Type = GetType(Double)
-            Me.excelDataSource1.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() {FieldInfo1, FieldInfo2, FieldInfo3, FieldInfo4, FieldInfo5, FieldInfo6, FieldInfo7, FieldInfo8, FieldInfo9, FieldInfo10})
-            ExcelWorksheetSettings1.CellRange = Nothing
-            ExcelWorksheetSettings1.WorksheetIndex = Nothing
-            ExcelWorksheetSettings1.WorksheetName = "Sheet1"
-            ExcelSourceOptions1.ImportSettings = ExcelWorksheetSettings1
-            Me.excelDataSource1.SourceOptions = ExcelSourceOptions1
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            fieldInfo1.Name = "CategoryName"
+            fieldInfo1.OriginalName = "CategoryName"
+            fieldInfo1.Type = GetType(String)
+            fieldInfo2.Name = "Country"
+            fieldInfo2.OriginalName = "Country"
+            fieldInfo2.Type = GetType(String)
+            fieldInfo3.Name = "ProductName"
+            fieldInfo3.OriginalName = "ProductName"
+            fieldInfo3.Type = GetType(String)
+            fieldInfo4.Name = "Sales Person"
+            fieldInfo4.OriginalName = "Sales Person"
+            fieldInfo4.Type = GetType(String)
+            fieldInfo5.Name = "OrderDate"
+            fieldInfo5.OriginalName = "OrderDate"
+            fieldInfo5.Type = GetType(System.DateTime)
+            fieldInfo6.Name = "OrderID"
+            fieldInfo6.OriginalName = "OrderID"
+            fieldInfo6.Type = GetType(Double)
+            fieldInfo7.Name = "Quantity"
+            fieldInfo7.OriginalName = "Quantity"
+            fieldInfo7.Type = GetType(Double)
+            fieldInfo8.Name = "Discount"
+            fieldInfo8.OriginalName = "Discount"
+            fieldInfo8.Type = GetType(Double)
+            fieldInfo9.Name = "Extended Price"
+            fieldInfo9.OriginalName = "Extended Price"
+            fieldInfo9.Type = GetType(Double)
+            fieldInfo10.Name = "UnitPrice"
+            fieldInfo10.OriginalName = "UnitPrice"
+            fieldInfo10.Type = GetType(Double)
+            Me.excelDataSource1.Schema.AddRange(New DevExpress.DataAccess.Excel.FieldInfo() {fieldInfo1, fieldInfo2, fieldInfo3, fieldInfo4, fieldInfo5, fieldInfo6, fieldInfo7, fieldInfo8, fieldInfo9, fieldInfo10})
+            excelWorksheetSettings1.CellRange = Nothing
+            excelWorksheetSettings1.WorksheetIndex = Nothing
+            excelWorksheetSettings1.WorksheetName = "Sheet1"
+            excelSourceOptions1.ImportSettings = excelWorksheetSettings1
+            Me.excelDataSource1.SourceOptions = excelSourceOptions1
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(800, 450)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
-
         Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private excelDataSource1 As DevExpress.DataAccess.Excel.ExcelDataSource
-	End Class
-End Namespace
 
+        Private excelDataSource1 As DevExpress.DataAccess.Excel.ExcelDataSource
+    End Class
+End Namespace
